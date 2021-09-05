08ad60348eba1ca96aef7e302bc3ce9472b26534
Lapis <45414333+Lapis256@users.noreply.github.com>
2021-09-06 01:50:35 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export * from "./src/utils.js";
+export { Event } from "./src/event.js";
+export { GameRule } from "./src/gamerule.js";
+export { Command } from "./src/command.js";
+export { Tick } from "./src/tick.js";
+export { Tag } from "./src/tag.js";
+export { Player } from "./src/player.js";
+export { Scoreboard } from "./src/scoreboard.js";
