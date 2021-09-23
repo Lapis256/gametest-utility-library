b00cd468a7a206fca7eb8bd2461ae8c7a398edcc
lapis256 <lapis128.256@gmail.com>
2021-09-23 16:00:41 +0900

    fix

--- src/object.js
+++ src/object.js
@@ -2,5 +2,7 @@ export function mergeObject(base, ...objects) {
     for(const obj of objects)
     for(const key in obj) {
         base[key] = obj[key];
+        const descriptor = Object.getOwnPropertyDescriptor(obj.constructor, key);
+        Object.defineProperty(base, key, {...descriptor, value: obj[key]});
     }
 }
